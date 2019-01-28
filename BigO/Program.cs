c4ee5c55306b4004b1809132e7802a7afa1fa99f
koufi <koufi@LAPTOP-QB0ITCT6>
2019-01-28 18:44:47 -0500

    HelloWorldTestV2

--- BigO/Program.cs
+++ BigO/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BigO
 {
@@ -10,6 +6,10 @@ namespace BigO
     {
         static void Main(string[] args)
         {
+            //Puause to display Hello world 
+            Console.WriteLine("Press any key to continue"); 
+            Console.WriteLine("Hello world");          
+            Console.ReadKey(true);
         }
     }
 }
